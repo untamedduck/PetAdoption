a5e31e0b05a2084e016127510a872243bb3d801e
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2023-03-03 13:30:14 -0700

    Added form functionality
    Added functionality to the form selection

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return(
+  <div>
+    <h1>Adopt Me!</h1>
+    <SearchParams />
+  </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
