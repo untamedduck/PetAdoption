012d0fa4555f61d2ba2884414f9c0a956e5ffdef
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2023-03-03 16:18:05 -0700

    Added pictured

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
-
+import Carousel from "./Carousel";
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
@@ -19,6 +19,7 @@ const Details = () => {
   return (
     <div className="details">
       <div>
+        <Carousel images={pet.images} />
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
         <button>Adopt {pet.name}</button>
